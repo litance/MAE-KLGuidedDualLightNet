0b23c107fa6f3a8c0f3d0efeec961a452b38f1c2
litance <leejm-wm24@student.tarc.edu.my>
2025-03-27 04:44:19 +0800

    Add STAM(SE Block) attention

--- main.py
+++ main.py
@@ -56,17 +56,36 @@ transform = transforms.Compose([
 dataset = ASLDataset(path, transform=transform)
 
 
+class SEBlock(nn.Module):
+    def __init__(self, in_channels, reduction=16):
+        super(SEBlock, self).__init__()
+        self.global_avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Linear(in_channels, in_channels // reduction)
+        self.fc2 = nn.Linear(in_channels // reduction, in_channels)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        batch, channels, _, _ = x.size()
+        y = self.global_avg_pool(x).view(batch, channels)
+        y = self.relu(self.fc1(y))
+        y = self.sigmoid(self.fc2(y))
+        y = y.view(batch, channels, 1, 1)
+        return x * y
+
 class MobileNetLSTMSTAM(nn.Module):
     def __init__(self, num_classes=len(dataset.classes)):
         super(MobileNetLSTMSTAM, self).__init__()
         self.mobilenet = models.mobilenet_v2(pretrained=True)
         self.mobilenet.features[18] = nn.Identity()
+        self.stam = SEBlock(320)
         self.lstm = nn.LSTM(320, 512, batch_first=True)
         self.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
         batch_size, C, H, W = x.shape
         x = self.mobilenet.features(x)
+        x = self.stam(x)
         x = x.mean([2, 3])
         x = x.view(batch_size, 1, -1)
         x, _ = self.lstm(x)
